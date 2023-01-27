6a190197a6a372ce9996d36f222ba42d02ac1e98
bruhlol108 <96218327+bruhlol108@users.noreply.github.com>
2023-01-27 15:56:13 -0600

    Update TeleOpDrive.java

--- Road Runner/TeleOpDrive.java
+++ Road Runner/TeleOpDrive.java
@@ -4,14 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
+import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "TeleOp_Mech")
 public class TeleOpDrive extends LinearOpMode {
 
+    private double ticksperRotation;
+    private Servo clawServo = null;
     private DcMotor motorFrontLeft = null;
     private DcMotor motorBackLeft = null;
     private DcMotor motorFrontRight = null;
     private DcMotor motorBackRight = null;
+    private DcMotor armMotor = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,13 +24,18 @@ public class TeleOpDrive extends LinearOpMode {
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackL");
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontR");
         DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackR");
-
+        DcMotor armMotor = hardwareMap.dcMotor.get("armMotor");
+        Servo clawServo = hardwareMap.servo.get("clawServo");
+        ticksperRotation = armMotor.getMotorType().getTicksPerRev();
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
         motorFrontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         motorFrontRight.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
 
@@ -46,6 +54,10 @@ public class TeleOpDrive extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
+            boolean dpad_up = gamepad2.dpad_up;
+            boolean dpad_down = gamepad2.dpad_down;
+            boolean dpad_left = gamepad2.dpad_left;
+            boolean dpad_right = gamepad2.dpad_right;
 
             double limit1 = 2;
             double limit2 = 1.2;
@@ -62,6 +74,12 @@ public class TeleOpDrive extends LinearOpMode {
                 backRightPower = (y + x - (rx/limit2)) / limit3;
             }
 
+            if(dpad_up) {
+                moveArm(.5, 800);
+            }
+            if(dpad_down) {
+                moveArm(-.5, -800);
+            }
             motorFrontLeft.setPower(frontLeftPower);
             motorBackLeft.setPower(backLeftPower);
             motorFrontRight.setPower(frontRightPower);
@@ -91,5 +109,16 @@ public class TeleOpDrive extends LinearOpMode {
             motorFrontRight.setPower(frontRightPower);
             motorBackRight.setPower(backRightPower);
         }
+    }
+    public void moveArm(double power, int distance) {
+        armMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        armMotor.setTargetPosition(distance);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setPower(power);
+        while(armMotor.isBusy()) {
+
+        }
+
+        armMotor.setPower(0);
     }
 }
